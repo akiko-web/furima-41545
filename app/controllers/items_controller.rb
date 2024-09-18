ebbb51cac0ffeaa269b7d3931b88c84d807221b4
akiko-web <ziakohappp@gmail.com>
2024-09-18 16:33:58 +0900

    ローカル環境でトップページまで表示

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,5 @@
+class ItemsController < ApplicationController
+  def index
+    # ここで必要に応じてデータを取得することも可能
+  end
+end
---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,5 @@
+class PrototypesController < ApplicationController
+  def index
+    # indexアクションに表示したい内容を書く
+  end
+end
