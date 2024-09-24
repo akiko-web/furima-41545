70788b9240c0b3c6435a43eedf6dac92640fb3ac
akiko-web <ziakohappp@gmail.com>
2024-09-24 11:59:21 +0900

    テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,20 @@ class Item < ApplicationRecord
   has_one_attached :image  
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :delivery_time_id, presence: true, numericality: { other_than: 0, message: "を選択してください" }
 
+  # presence: true必須
+  validates :item_name,     presence: true
+  validates :description,   presence: true
+  validates :image,   presence: true
+  validates :price, presence: true, numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 300, 
+    less_than_or_equal_to: 9_999_999, 
+    message: 'は300円から9,999,999円の間で設定してください' 
+  }
+  validates :delivery_time_id, presence: true, numericality: { other_than: 0, message: "を選択してください" }
+  validates :category_id,     presence: true, numericality: { other_than: 0, message: "を選択してください" }
+  validates :region_id,       presence: true, numericality: { other_than: 0, message: "を選択してください" }
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 0, message: "を選択してください" }
+  validates :condition_id,    presence: true, numericality: { other_than: 0, message: "を選択してください" }
 end
