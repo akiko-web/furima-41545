c0022ccd7f95ff5e3fc9ddca7b55d279f5d0ad4d
akiko-web <ziakohappp@gmail.com>
2024-10-03 16:41:48 +0900

    消去

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
 
   def new
     @item = Item.new
