140d066be9ed5f3e32a1aae56ab523b0b3676cf0
akiko-web <ziakohappp@gmail.com>
2024-10-03 16:56:17 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth  # Basic認証を呼び出す
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+  end
+
+  private
+
+  # Basic認証のメソッドを定義
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
 end
